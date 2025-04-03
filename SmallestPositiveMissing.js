a6c64b32aa41475fec7839cd47310ab7f5254c19
ShakhawatHossain <88829916+Shakhawat1998@users.noreply.github.com>
2025-04-04 01:51:49 +0600

    new problems added 25

---
+++ SmallestPositiveMissing.js
@@ -0,0 +1,40 @@
+// You are given an integer array arr[]. 
+// Your task is to find the smallest 
+// positive number missing from the array.
+
+// Note: Positive number starts 
+// from 1. The array can have 
+// negative integers too.
+
+// Examples:
+
+// Input: arr[] = [2, -3, 4, 1, 1, 7]
+// Output: 3
+// Explanation: Smallest positive 
+// missing number is 3.
+// Input: arr[] = [5, 3, 2, 5, 1]
+// Output: 4
+// Explanation: Smallest positive 
+// missing number is 4.
+// Input: arr[] = [-8, 0, -1, -4, -3]
+// Output: 1
+// Explanation: Smallest positive missing 
+// number is 1.
+
+function missingNumber(arr) {
+    arr.sort((a, b) => a - b);
+    let smallest = 1;
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === smallest) {
+            smallest++;
+        }
+    }
+
+    return smallest;
+}
+
+// Example usage:
+console.log(missingNumber([2, -3, 4, 1, 1, 7])); 
+console.log(missingNumber([5, 3, 2, 5, 1]));    
+console.log(missingNumber([-8, 0, -1, -4, -3])); 
